0caaf5cf00f7e850aa6fd4ce266c5fef89cabb3b
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2024-03-11 20:22:41 +0200

    setup project

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <p>React project template by</p>
+      <a
+        href="https://github.com/Sheremeta-Ivan?tab=repositories"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ marginLeft: 20, color: '#007bff', display: 'inline-block' }}
+      >
+        Sheremeta Ivan
+      </a>
+    </div>
+  );
+};
+
+export default App;
